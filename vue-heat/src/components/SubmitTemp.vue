4dea4a9c66ab6a945d76712f408557eda2f98744
aceagles <ad.c.eagles@gmail.com>
2021-09-28 21:44:01 +0100

    Add Axios, create submit temperature component

---
+++ vue-heat/src/components/SubmitTemp.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="sbmt">
+      Temperature: <input type="number" v-model="temp_hum.temperature"/>
+      <br>
+      Humidity: <input type="number" v-model="temp_hum.humidity"/>
+      <br>
+      <input type="submit" value="submit" />
+  </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            temp_hum: {
+                temperature:  0,
+                humidity: 0
+            }
+        }
+    },
+    methods: {
+        sbmt () {
+            alert("Clicked")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
